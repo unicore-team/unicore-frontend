4092c0610be0b1d3991ef7c820e85fd641af3b2a
inf0craw1 <inf0craw1@naver.com>
2025-06-06 22:10:08 +0900

    feat: Add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,33 @@
-import localFont from "next/font/local";
-import type { Metadata } from "next";
-import "@/styles/global.scss";
-import Header from "@/components/Header";
+import localFont from 'next/font/local';
+import type { Metadata } from 'next';
+import '@/styles/global.scss';
+import Header from '@/components/Header';
 
 const pretendard = localFont({
-  variable: "--font-pretendard",
-  display: "swap",
+  variable: '--font-pretendard',
+  display: 'swap',
   src: [
     {
-      path: "../fonts/Pretendard/Pretendard-Medium.woff2",
-      weight: "500",
-      style: "normal",
+      path: '../fonts/Pretendard/Pretendard-Medium.woff2',
+      weight: '500',
+      style: 'normal',
     },
     {
-      path: "../fonts/Pretendard/Pretendard-Bold.woff2",
-      weight: "700",
-      style: "normal",
+      path: '../fonts/Pretendard/Pretendard-Bold.woff2',
+      weight: '700',
+      style: 'normal',
     },
   ],
 });
 
 export const metadata: Metadata = {
-  title: "UNICORE - 핵심을 꿰뚫는 특별함",
-  description: "프리미엄 웹 제작의 시작, UNICORE",
+  title: 'UNICORE - 핵심을 꿰뚫는 특별함',
+  description: '프리미엄 웹 제작의 시작, UNICORE',
+  icons: {
+    icon: '/favicon-32x32.png',
+    shortcut: '/favicon.ico',
+    apple: '/apple-touch-icon.png', // 선택
+  },
 };
 
 export default function RootLayout({
