df66e6c431c3d9fe33a4f19cf7bb7c87c5ae33bd
inf0craw1 <inf0craw1@naver.com>
2025-06-17 17:21:10 +0900

    feat: Add workItems

--- src/components/sections/WorkSection.tsx
+++ src/components/sections/WorkSection.tsx
@@ -1,4 +1,6 @@
 import styles from '@/components/sections/WorkSection.module.scss';
+import WorkItem from '@/components/WorkItem';
+import { workItemData } from '@/data/workItemData';
 
 const WorkSection = () => {
   return (
@@ -10,6 +12,16 @@ const WorkSection = () => {
           <br />
           개발은 <span className={styles.gradient}>견고</span>하게
         </h2>
+      </div>
+      <div className={styles.workList}>
+        {workItemData.map((item, index) => (
+          <WorkItem
+            key={`WorkItem-${item.id}`}
+            thumbnail={item.thumbnail}
+            title={item.title}
+            description={item.description}
+          />
+        ))}
       </div>
     </section>
   );
