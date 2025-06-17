40c1b9022dfcb08a3214cd7c8eae9e1313c6e80a
inf0craw1 <inf0craw1@naver.com>
2025-06-17 17:20:40 +0900

    feat: Add WorkItem

---
+++ src/components/WorkItem.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image';
+import styles from './WorkItem.module.scss';
+
+type WorkItemProps = {
+  thumbnail: string;
+  title: string;
+  description: string;
+};
+
+const WorkItem = ({ thumbnail, title, description }: WorkItemProps) => {
+  return (
+    <div className={styles.workItem}>
+      <div className={styles.thumbnailWrapper}>
+        <Image
+          src={thumbnail}
+          alt={title}
+          fill
+          className={styles.thumbnail}
+          sizes="100%"
+          priority
+        />
+      </div>
+      <div className={styles.textWrapper}>
+        <h3 className={`${styles.title} title1 mobile-title2`}>{title}</h3>
+        <p className={`${styles.description} body2`}>{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default WorkItem;
