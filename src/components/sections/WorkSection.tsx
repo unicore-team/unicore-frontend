677fe26f4eb5880acbf4fe577f99219062fc8acb
inf0craw1 <inf0craw1@naver.com>
2025-06-17 18:31:56 +0900

    feat: Add inner for max width

--- src/components/sections/WorkSection.tsx
+++ src/components/sections/WorkSection.tsx
@@ -5,23 +5,25 @@ import { workItemData } from '@/data/workItemData';
 const WorkSection = () => {
   return (
     <section className={styles.workSection}>
-      <div className={styles.titleWrapper}>
-        <h4 className="title1 mobile-body1">OUR WORK</h4>
-        <h2 className="heading1 mobile-heading2">
-          디자인은 <span className={styles.gradient}>감각적</span>으로
-          <br />
-          개발은 <span className={styles.gradient}>견고</span>하게
-        </h2>
-      </div>
-      <div className={styles.workList}>
-        {workItemData.map((item, index) => (
-          <WorkItem
-            key={`WorkItem-${item.id}`}
-            thumbnail={item.thumbnail}
-            title={item.title}
-            description={item.description}
-          />
-        ))}
+      <div className={styles.inner}>
+        <div className={styles.titleWrapper}>
+          <h4 className="title1 mobile-body1">OUR WORK</h4>
+          <h2 className="heading1 mobile-heading2">
+            디자인은 <span className={styles.gradient}>감각적</span>으로
+            <br />
+            개발은 <span className={styles.gradient}>견고</span>하게
+          </h2>
+        </div>
+        <div className={styles.workList}>
+          {workItemData.map((item, index) => (
+            <WorkItem
+              key={`WorkItem-${item.id}`}
+              thumbnail={item.thumbnail}
+              title={item.title}
+              description={item.description}
+            />
+          ))}
+        </div>
       </div>
     </section>
   );
