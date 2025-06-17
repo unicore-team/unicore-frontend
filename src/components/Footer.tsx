aa50490c4f17bb6c0fbb7ebe620b8926274a81b6
inf0craw1 <inf0craw1@naver.com>
2025-06-17 18:35:43 +0900

    fix: Edit css

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,12 +4,12 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
-        <p className={`${styles.contactText} title2 body2-mobile`}>
+        <p className={`${styles.contactText} title2 mobile-body2`}>
           비즈니스 문의는 아래 메일로 연락 부탁드립니다 :)
         </p>
-        <p className={`${styles.email} heading1 heading2-mobile`}>contact@unicore.kr</p>
+        <p className={`${styles.email} heading1 mobile-heading2`}>contact@unicore.kr</p>
 
-        <p className={`${styles.companyInfo} body2 caption-mobile`}>
+        <p className={`${styles.companyInfo} body2 mobile-caption`}>
           유니코어 | 대표자 박찬진 | 사업자등록번호 104-14-22486
           <br />
           COPYRIGHT © UNICORE CO., LTD ALL RIGHTS RESERVED.
